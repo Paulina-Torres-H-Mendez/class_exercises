bdd395bd33090dc3a1756b1017955aeedf8e45d9
paulinatorresh <paulimela@icloud.com>
2025-02-25 20:37:15 +0100

    Class exercises previously not on github as there was an issue with uploading

---
+++ dictionary.py
@@ -0,0 +1,18 @@
+d = {} # empty dictionary
+eng_to_spa = {"one":"uno", "two": "dos", "three": "tres"}
+print(eng_to_spa)
+eng_to_spa["i"] = "yo"
+eng_to_spa["am"] = "soy"
+eng_to_spa["spanish"] = "español"
+print(eng_to_spa)
+sentence = "i am spanish"
+words = sentence.split()
+for word in words:
+    print(eng_to_spa[word])
+eng_to_spa.update({"yes": "si", "no" : "no"})
+print(eng_to_spa)
+del eng_to_spa["no"]
+print(eng_to_spa)
+
+for key in eng_to_spa:
+    print(f"{eng_to_spa[key]} means {key}")
